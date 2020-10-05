3068c2415ae34c6e31ec718f33c6e1c77cb19a9d
pichda <pichnarcikdani3l@gmail.com>
2020-10-05 13:49:17 +0200

    cviceni 1

---
+++ ulohy/Cviceni01/Excercise1/Sample01/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+struct Triangle{
+	int a, b, c;
+};
+
+bool canExist(Triangle* t) {
+	if (t->a + t->b > t->c && t->a + t->c > t->b && t->c + t->b > t->a) {
+		return true;
+	}
+	else return false;
+}
+
+int main(int argc, char** argv) 
+{
+	int countTriangle;
+	std::cout << "Zadej pocet trojuhelniku:";
+	std::cin >> countTriangle;
+	Triangle* triangles = new Triangle[countTriangle];
+	for (int i = 0; i < countTriangle; i++)
+	{
+		std::cout << "Zadej strany pro trojuhelnik:"<< i+1;
+		std::cout << "\nZadej stranu a=";
+		std::cin >> triangles[i].a;
+		std::cout << "\nZadej stranu b=";
+		std::cin >> triangles[i].b;
+		std::cout << "\nZadej stranu c=";
+		std::cin >> triangles[i].c;
+		std::cout << "\n";
+
+	}
+	delete[] triangles;
+	return 0;
+}
