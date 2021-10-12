7fd1661558920ea3b212862145286b1d5775cb02
pichda <72087407+pichda@users.noreply.github.com>
2021-10-12 09:35:59 +0200

    Update Main.cpp

--- ulohy/Exercise2/Exercise2/Main.cpp
+++ ulohy/Exercise2/Exercise2/Main.cpp
@@ -4,19 +4,31 @@
 
 int main(int argc, char** argv)
 {
-	CashRegister* cashRegist = new CashRegister();
+		CashRegister* cashRegist = new CashRegister{};
 
-	cashRegist->CreateTicket(100, 20);
-	cashRegist->CreateTicket(1000, 50);
-	cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(100, 20);
+		cashRegist->CreateTicket(1000, 50);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
+		cashRegist->CreateTicket(60, 100);
 
-	std::cout << "cost of ticket 1001:" << cashRegist->GetTicket(1000).GetCost() << "\n";
-	std::cout << "cost of ticket 1002:" << cashRegist->GetTicket(1001).GetCost() << "\n";
-	std::cout << "cost of ticket 1003:" << cashRegist->GetTicket(1002).GetCost() << "\n";
 
-	std::cout << "cost of tickets:" << cashRegist->GetCost() << "\n";
-	std::cout << "cost of tickets with tax:" << cashRegist->GetCostInDph() << "\n";
+		std::cout << "cost of ticket 1001:" << cashRegist->GetTicket(1000).GetCost() << "\n";
+		std::cout << "cost of ticket 1002:" << cashRegist->GetTicket(1001).GetCost() << "\n";
+		std::cout << "cost of ticket 1003:" << cashRegist->GetTicket(1002).GetCost() << "\n";
 
+		std::cout << "cost of tickets:" << cashRegist->GetCost() << "\n";
+		std::cout << "cost of tickets with tax:" << cashRegist->GetCostInDph() << "\n";
+
+		delete cashRegist;
+
+		std::cout << "\nkonec programu.";
 	return 0;
 
 }
